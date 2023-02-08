6d9ed103213b571f11e7cd9e4b4d09f98f18352e
Vanessa Mitelman <vanessamitelman@gmail.com>
2023-02-08 08:58:11 +0200

    loader and ubuntu

--- src/assets/wrappers/LoaderWrapper.js
+++ src/assets/wrappers/LoaderWrapper.js
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
 
 const LoaderWrapper = styled.div`
-  display: flex;
-  justify-content: center;
   @keyframes turn {
     0% {
       transform: rotate(0deg);
@@ -13,33 +11,30 @@ const LoaderWrapper = styled.div`
   }
   #inner {
     border-radius: 50%;
-    width: 150px;
-    height: 150px;
+    width: 50vh;
+    height: 50vh;
     border: 4px solid transparent;
     border-top-color: #000;
     border-bottom-color: #000;
     position: absolute;
-    top: 10px;
-    left: 10px;
     animation: turn 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s infinite;
   }
 
   #outer {
     border-radius: 50%;
-    width: 170px;
-    height: 170px;
+    width: 45vh;
+    height: 45vh;
     border: 4px solid transparent;
     border-left-color: #000;
     border-right-color: #000;
     position: absolute;
-    top: 0;
-    left: 0;
     animation: turn 3s cubic-bezier(0.785, 0.135, 0.15, 0.86) 0s infinite;
   }
   #spinner {
     position: relative;
-    top: 4rem;
-    left: -90px;
+    display: grid;
+    place-items: center;
+    height: 60vh;
   }
 `;
 
--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import App from './App';
 import './i18n';
 import { store } from './store';
 import { Provider } from 'react-redux';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
