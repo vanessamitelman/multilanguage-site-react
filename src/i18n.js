8ae5a622c2f2351add625148d6163b0075f9fca3
Vanessa Mitelman <vanessamitelman@gmail.com>
2023-02-12 13:10:28 +0200

    moved translation
    moved translation to backend

--- src/i18n.js
+++ src/i18n.js
@@ -1,52 +1,19 @@
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from 'i18next-http-backend';
 
 const i18n = i18next.createInstance();
 
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
+  .use(Backend)
   .init({
     debug: false,
     fallbackLng: 'en',
     interpolation: {
       escapeValue: false
-    },
-    resources: {
-      en: {
-        translation: {
-          nav: {
-            home: 'Home',
-            blog: 'News'
-          },
-          footer:
-            'Vanessa is a registered trademark of Vanessa Ltd. and its affiliates.',
-          banner: 'A new website is coming...',
-          back: 'back',
-          missing_data: 'No information available right now',
-          error_page1: 'error 404',
-          error_page2: 'Oops, you have reached a page that does not exist',
-          language_selector: 'Select language',
-          readMore: 'Read article'
-        }
-      },
-      he: {
-        translation: {
-          nav: {
-            home: 'בית',
-            blog: 'חדשות'
-          },
-          footer: 'Vanessa הוא סימן מסחרי רשום של Vanessa Ltd. ושל החברות שלה.',
-          banner: 'אתר חדש מגיע',
-          back: 'חזרה',
-          missing_data: 'אין מידע להציג כרגע',
-          error_page1: 'שגיאה 404',
-          error_page2: 'אופס, טעות בדף',
-          language_selector: 'בחר שפה',
-          readMore: 'קרא כתבה'
-        }
-      }
     }
   });
 
