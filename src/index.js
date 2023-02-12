6bc008bb750b302d23ebc5cac122acc97d00d357
Vanessa Mitelman <vanessamitelman@gmail.com>
2023-02-12 13:20:48 +0200

    changed footer text

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import './i18n';
-import { store } from './store';
-import { Provider } from 'react-redux';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import App from './App';
+import './i18n';
+import { store } from './store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <App />
+    <Suspense fallback='loading'>
+      <App />
+    </Suspense>
   </Provider>
 );
